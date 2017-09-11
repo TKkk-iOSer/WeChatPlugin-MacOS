63980698ea49f501f6da7a95ff38647b107ee61b
tk <1049893361@qq.com>
2017-09-11 11:58:37 +0800

    Update ==> Build Phases/Run Script

---
+++ Other/Products/Debug/WeChatPlugin.framework/Versions/Current/Headers/WeChatPlugin.h
@@ -0,0 +1,92 @@
+//
+//  WeChatPlugin.h
+//  WeChatPlugin
+//
+//  Created by TK on 2017/4/19.
+//  Copyright © 2017年 tk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "TKWeChatPluginConfig.h"
+#import "TKHelper.h"
+
+FOUNDATION_EXPORT double WeChatPluginVersionNumber;
+FOUNDATION_EXPORT const unsigned char WeChatPluginVersionString[];
+
+#pragma mark - 微信原始的部分类与方法
+
+@interface MMMainWindowController : NSObject
+- (void)onAuthOK;
+- (void)onLogOut;
+@end
+
+@interface MessageService : NSObject
+- (void)onRevokeMsg:(id)arg1;
+- (void)OnSyncBatchAddMsgs:(NSArray *)arg1 isFirstSync:(BOOL)arg2;
+- (id)SendTextMessage:(id)arg1 toUsrName:(id)arg2 msgText:(id)arg3 atUserList:(id)arg4;
+- (id)GetMsgData:(id)arg1 svrId:(long)arg2;
+- (void)AddLocalMsg:(id)arg1 msgData:(id)arg2;
+@end
+
+@interface MMServiceCenter : NSObject
++ (id)defaultCenter;
+- (id)getService:(Class)arg1;
+@end
+
+@interface SKBuiltinString_t : NSObject
+@property(retain, nonatomic, setter=SetString:) NSString *string; // @synthesize string;
+@end
+
+@interface AddMsg : NSObject
+@property(retain, nonatomic, setter=SetContent:) SKBuiltinString_t *content; // @synthesize content;
+@property(retain, nonatomic, setter=SetFromUserName:) SKBuiltinString_t *fromUserName; // @synthesize fromUserName;
+@property(nonatomic, setter=SetMsgType:) int msgType; // @synthesize msgType;
+@property(retain, nonatomic, setter=SetToUserName:) SKBuiltinString_t *toUserName; // @synthesize toUserName;
+@property (nonatomic, assign) unsigned int createTime;
+@end
+
+@interface WeChat : NSObject
++ (id)sharedInstance;
+@end
+
+@interface ContactStorage : NSObject
+- (id)GetSelfContact;
+- (id)GetContact:(id)arg1;
+@end
+
+@interface WCContactData : NSObject
+@property(retain, nonatomic) NSString *m_nsUsrName; // @synthesize m_nsUsrName;
+@property(nonatomic) unsigned int m_uiFriendScene;  // @synthesize m_uiFriendScene;
+@end
+
+@interface MessageData : NSObject
+- (id)initWithMsgType:(long long)arg1;
+@property(retain, nonatomic) NSString *fromUsrName;
+@property(retain, nonatomic) NSString *toUsrName;
+@property(retain, nonatomic) NSString *msgContent;
+@property(retain, nonatomic) NSString *msgPushContent;
+@property(nonatomic) int messageType;
+@property(nonatomic) int msgStatus;
+@property(nonatomic) int msgCreateTime;
+@property(nonatomic) int mesLocalID;
+@end
+
+@interface CUtility : NSObject
++ (BOOL)HasWechatInstance;
++ (unsigned long long)getFreeDiskSpace;
+@end
+
+#pragma mark - 调用 NSSearchPathForDirectoriesInDomains 的一些方法
+@interface PathUtility : NSObject
++ (id)getSysCachePath;
++ (id)getSysDocumentPath;
++ (id)getSysLibraryPath;
+@end
+
+@interface MemoryMappedKV : NSObject
++ (id)mappedKVPathWithID:(id)arg1;
+@end
+
+@interface JTStatisticManager : NSObject
+@property(retain, nonatomic) NSString *statFilePath; 
+@end
